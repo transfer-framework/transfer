9038a6c068cf9b91c42a7f3452f7bd024dbc1c57
Valentin Jonovs <valentin@jonovs.com>
2015-10-13 16:38:58 +0200

    Remove unused use statements

--- src/Transfer/Console/Command/Manifest/DescribeCommand.php
+++ src/Transfer/Console/Command/Manifest/DescribeCommand.php
@@ -9,7 +9,6 @@
 
 namespace Transfer\Console\Command\Manifest;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
